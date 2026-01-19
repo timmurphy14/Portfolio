823c83640e07f588cfb782aac76ef7254ae0b490
Tim Murphy <timmurphy@Tims-MacBook-Air.local>
2026-01-19 18:23:13 -0500

    Load raw contextual and weekly tracking data

--- pbu-coverage-model/src/01_load_data.R
+++ pbu-coverage-model/src/01_load_data.R
@@ -4,7 +4,7 @@
 source("src/00_load_packages.R")
 
 #Contextual data
-raw_context <- read_csv("supplementary_data.csv", show_col_types = FALSE)
+raw_context <- read_csv("data/supplementary_data.csv", show_col_types = FALSE)
 
 #Weeks to load
 weeks <- sprintf("%02d", 1:18)
@@ -16,12 +16,12 @@ raw_outputs <- list()
 #Load weekly tracking data
 for (w in weeks) {
   raw_inputs[[w]]  <- read_csv(
-    paste0("input_2023_w", w, ".csv"), 
+    paste0("data/input_2023_w", w, ".csv"), 
     show_col_types = FALSE
     )
   
   raw_outputs[[w]] <- read_csv(
-    paste0("output_2023_w", w, ".csv"), 
+    paste0("data/output_2023_w", w, ".csv"), 
     show_col_types = FALSE
     )
 }
